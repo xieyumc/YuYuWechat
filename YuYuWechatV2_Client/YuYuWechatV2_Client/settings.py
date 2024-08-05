f05993e0a229bf52d7a41c99b7ccded776c951c8
XieYu <xieyumc@gmail.com>
2024-08-05 20:54:36 +0800

    refactor：数据库切换到postgresql

--- YuYuWechatV2_Client/YuYuWechatV2_Client/settings.py
+++ YuYuWechatV2_Client/YuYuWechatV2_Client/settings.py
@@ -62,10 +62,15 @@ WSGI_APPLICATION = "YuYuWechatV2_Client.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB', 'postgres'),
+        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'tykWyr-bepqu6-fafvym'),
+        'HOST': os.environ.get('POSTGRES_HOST', '127.0.0.1'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5431'),
     }
+
 }
 
 # Password validation
