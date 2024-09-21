2d68c52bef503d44bc4fa8fe80dc02c795514315
XieYu <xieyumc@gmail.com>
2024-09-21 15:36:12 +0800

    fix：修复服务端后台管理界面css缺失

--- YuYuWechatV2_Server/YuYuWechatV2/settings.py
+++ YuYuWechatV2_Server/YuYuWechatV2/settings.py
@@ -122,7 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # 用于存放 collectstatic 收集的静态文件
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- YuYuWechatV2_Server/dist/YuYuWechatV2_Server_run.bat
+++ YuYuWechatV2_Server/dist/YuYuWechatV2_Server_run.bat
@@ -3,4 +3,4 @@ REM 获取当前目录
 set current_dir=%~dp0
 
 REM 运行可执行文件并添加参数
-"%current_dir%YuYuWechatV2_Server.exe" runserver 0.0.0.0:8000
+"%current_dir%YuYuWechatV2_Server.exe" runserver 0.0.0.0:8000 --insecure
